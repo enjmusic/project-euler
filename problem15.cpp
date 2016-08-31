03bbd5d55b53409218729fa085243c945dae37fc
Ian Hoffman <ijh6@cornell.edu>
2016-08-30 22:04:52 -0400

    Almost done with BigInt

--- problem15.cpp
+++ problem15.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using std::string;
 using std::to_string;
+using std::max;
 using std::cout;
 using std::endl;
 
@@ -9,6 +11,10 @@ using std::endl;
 class BigInt {
 	string number;
 public:
+	BigInt() {
+		this->number = "";
+	}
+
 	BigInt(int i) {
 		this->number = to_string(i);
 	}
@@ -17,8 +23,59 @@ public:
 		cout << this->number << endl;
 	}
 
+	BigInt operator+(const BigInt &b) {
+		auto result = BigInt();
+
+		auto thisIter = this->number.rbegin();
+		auto otherIter = b.number.rbegin();
+
+		int sum = 0;
+		do {
+			if (thisIter != this->number.rend()) {
+				sum += (*thisIter - '0');
+				thisIter++;
+			}
+			if (otherIter != b.number.rend()) {
+				sum += (*otherIter - '0');
+				otherIter++;
+			}
+
+			result.number.push_back(sum%10 + '0');
+			sum /= 10;
+		} while (sum || 
+			thisIter != this->number.rend() ||
+			otherIter != b.number.rend());
+
+		// Remove extra zeroes 
+		// while (result.number.back() == 0) {
+		// 	result.number.pop_back();
+		// }
+
+		string reversed;
+		for (auto it = result.number.rbegin(); it != result.number.rend(); ++it) {
+			reversed.push_back(*it);
+		}
+		result.number = reversed;
+
+		return result;
+	}
+
 	BigInt operator*(int b) {
-		return *this;
+		auto result = BigInt();
+
+		int place = 0;
+		auto it = this->number.rbegin();
+		for (; it != this->number.rend(); ++it) {
+			auto thisPlace = BigInt((*it - '0') * b);
+			for (int i = 0; i < place; i++) {
+				thisPlace.number.push_back('0');
+			}
+
+			result = result + thisPlace;
+			place++;
+		}
+
+		return result;
 	}
 
 	BigInt operator/(int b) {
@@ -46,5 +103,7 @@ int main(int argc, char** argv) {
 	//			 20! * 20!
 	//
 
-	factOverNSquared(40, 20).print();
+	// factOverNSquared(40, 20).print();
+	// (BigInt(29) + BigInt(11)).print();
+	(BigInt(21) * 3).print();
 }
