7a10956669b0cdd0540a9adbc1da0ee74b15b887
Ian Hoffman <ijh6@cornell.edu>
2016-09-02 12:57:04 -0400

    Argh

--- solutions/problem20.cpp
+++ solutions/problem20.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <ctime> // Timing implementation
+using std::clock_t;
+using std::clock;
 using std::cout;
 using std::endl;
 using std::vector;
 #include "../headers/bigint.h"
 
 int main(int argc, char** argv) {
+	clock_t start;
+ 	double duration;
+	start = clock();
+
 	auto n = BigInt(2);
 	for (int i = 3; i <= 100; i++) {
 		n = n * BigInt(i);
@@ -15,6 +22,10 @@ int main(int argc, char** argv) {
 	for (auto it = v.begin(); it != v.end(); ++it) {
 		sum += *it;
 	}
+
+	duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
 	cout << "100! = "; n.print();
-	cout << "Sum of its digits: " << sum << endl;
+
+	cout << "Sum of its digits: " << sum;
+	cout << " calculated in " << duration << " seconds." << endl;
 }
