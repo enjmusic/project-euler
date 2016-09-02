b6c13b193722fb7279174ddf71f0df63720058f6
Ian Hoffman <ijh6@cornell.edu>
2016-09-02 12:40:20 -0400

    Added proudness comment

--- solutions/problem24.cpp
+++ solutions/problem24.cpp
@@ -7,6 +7,22 @@ using std::cout;
 using std::endl;
 using std::vector;
 
+/*
+ * 
+ * Very happy with this solution, takes
+ * O(n) space and O(n) time, where n is
+ * the number of digits (10 here)
+ *
+ * For this problem, this reduces to
+ * the same amount of time for any
+ * convolution index, 1 through 10!
+ *
+ * This makes calculation significantly
+ * faster than creating a 10!-length list
+ * of convolutions and then sorting it
+ *
+ */
+
 #define CONVOLUTION_NUMBER 1000000
 
 // Factorial
