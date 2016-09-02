9e62c3754351ea68673067a4fe7d608c1327bccb
Ian Hoffman <ijh6@cornell.edu>
2016-09-02 00:16:05 -0400

    Parsing file was successful

--- problem22.cpp
+++ problem22.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <algorithm>
 using std::cout;
 using std::endl;
 using std::vector;
+using std::ifstream;
 #include "bigint.h"
 
 int main(int argc, char** argv) {
-	
+	ifstream file("downloaded_files/p022_names.txt");
+	vector<string> names;
+	string name;
+
+	while (file.good()) {
+		getline(file, name, ',');
+		name.erase(
+			remove(name.begin(), name.end(), '\"'),
+			name.end()
+			);
+		names.push_back(name);
+	}
+
+	for (auto it = names.begin(); it != names.end(); ++it) {
+		cout << *it << endl;
+	}
 }
