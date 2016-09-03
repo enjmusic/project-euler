6ae2212350ebfe88e084f6d1c17b893e42acfb28
Ian Hoffman <ijh6@cornell.edu>
2016-09-03 14:15:32 -0400

    Decided to implement Ukkonen suffix trees because why not?

---
+++ headers/suffixtree.h
@@ -0,0 +1,30 @@
+//
+// suffixtree.h
+//
+// Ian Hoffman
+//
+// 9/3/16
+//
+
+#ifndef SUFFIXTREE_H
+#define SUFFIXTREE_H
+
+class STEdge {
+
+public:
+
+};
+
+class STNode {
+
+public:
+
+};
+
+class SuffixTree {
+	STNode root;
+public:
+
+};
+
+#endif
