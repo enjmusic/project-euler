103658a0b8825a291c1f8d577b210d91aad783d4
Ian Hoffman <ijh6@cornell.edu>
2016-09-03 11:39:06 -0400

    Long division works, time for cycle finding

--- solutions/problem26.cpp
+++ solutions/problem26.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <ctime> // Timing implementation
-#include <vector>
+#include <string>
 using std::clock_t;
 using std::clock;
 using std::cout;
 using std::endl;
-using std::vector;
+using std::string;
 
 int main(int argc, char** argv) {
 	clock_t start;
@@ -15,7 +15,18 @@ int main(int argc, char** argv) {
 	int currD = 7;
 	int currLongest = 6;
 	for (int i = 11; i < 1000; i++) {
-
+		string currDecimal;
+		int proc = 10;
+		for (int j = 0; j < 100 && proc; j++) {
+			if (proc < i) {
+				proc *= 10;
+				currDecimal.push_back('0');
+			} else {
+				currDecimal.push_back('0' + proc/i);
+				proc %= i;
+			}
+		}
+		cout << "1/" << i << " = 0." << currDecimal << endl;
 	}
 
 	duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
